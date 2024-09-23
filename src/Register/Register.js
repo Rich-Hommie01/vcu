2209b884af738a7de6878a142b52976b30598792
Rich-Hommie <richhommie160@aol.com>
2024-09-23 10:48:40 -0700

    i forgot to add this

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -110,7 +110,7 @@ const Register = () => {
     if (validateStep()) {
       setIsLoading(true);
       try {
-        const response = await fetch('http://localhost:5000/api/auth/signup', {
+        const response = await fetch('https://backend-av3s.onrender.com/api/auth/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
